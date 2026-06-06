4e684a98ae0c6dcdb126f1670afbe231b76254cb
indy <dork@indypaige.dev>
2026-06-05 19:40:21 -0500

    remove random color schemes

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,16 +1,4 @@
 { pkgs, ... }: {
-        colorScheme = "dark";
-
-        iconTheme   = {
-          package = pkgs.adwaita-icon-theme;
-          name    = "Adwaita";
-        };
-
-        theme       = {
-          package = pkgs.flat-remix-gtk;
-          name    = "Flat-Remix-GTK-Violet-Darkest";
-        };
-
         font        = {
           name = "Iosevka";
           size = 10;
